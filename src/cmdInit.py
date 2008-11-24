338224b3469ce56a6e9c8cad056458fe67908e80
Francesco Salvestrini <salvestrini@gmail.com>
2008-11-25 00:06:44 +0100

    Fixed arguments handling

--- src/cmdInit.py
+++ src/cmdInit.py
@@ -37,7 +37,8 @@ def do(configuration, arguments) :
 
     (opts, args) = command.parse_args(arguments)
 
-    if (opts.force == False) :
+    if (opts.force != True) :
+        debug("Force mode disabled")
 	if (os.path.isfile(DEFAULT_DB_FILE)) :
 	    raise Exceptions.ForceNeeded("database already exists")
 	return 1
