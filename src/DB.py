8a3fbd2868f1338bf83203e63aeefe6eb95edc41
Francesco Salvestrini <salvestrini@gmail.com>
2008-12-03 20:22:10 +0100

    Rearranged DB class

--- src/DB.py
+++ src/DB.py
@@ -52,14 +52,14 @@ def fromxml(xml) :
 
     if (xml.tag == "entry") :
 	text     = xml.text
-        try :
-            priority = string_to_priority(xml.attrib['priority'])
-        except :
-            priority = Entry.PRIORITY_MEDIUM
-        try :
-            time     = xml.attrib['time']
-        except :
-            time     = datetime.date.today()
+	try :
+	    priority = string_to_priority(xml.attrib['priority'])
+	except :
+	    priority = Entry.PRIORITY_MEDIUM
+	try :
+	    time     = xml.attrib['time']
+	except :
+	    time     = datetime.date.today()
     else :
 	raise Exceptions.Database("unknown element `" + xml.tag + "'")
 
@@ -67,7 +67,7 @@ def fromxml(xml) :
 
     j = 0
     for x in xml.getchildren() :
-	#debug("Working with child")
+	debug("Working with child `" + str(x) + "'")
 	tmp = fromxml(x)
 	if (tmp != None) :
 	    entry.child(j, tmp)
@@ -85,7 +85,7 @@ def toxml(node, xml) :
 	return
 
     child = ET.SubElement(xml,
-                          tag = "entry")
+			  tag = "entry")
 #    ,
 #                          attrib = {
 #            'priority' : string_to_priority(node.priority),
@@ -110,15 +110,15 @@ class Database :
 
 	try :
 	    xml  = ET.parse(name)
-            assert(xml != None)
+	    assert(xml != None)
 	    root = xml.getroot()
-            assert(root != None)
-            if (len(root) > 1) :
-                raise Exceptions.Database("malformed database format " +
-                                          "in file `" + name + "')")
-            root = root[0]
+	    assert(root != None)
+	    if (len(root) > 1) :
+		raise Exceptions.Database("malformed database format " +
+					  "in file `" + name + "')")
+	    root = root[0]
 
-            #ET.dump(root)
+	    #ET.dump(root)
 
 	except IOError, e :
 	    raise Exceptions.Database("problems reading database " +
@@ -131,7 +131,12 @@ class Database :
 
 	assert(xml != None)
 
-	return fromxml(root)
+        tree = fromxml(root)
+        assert(tree != None)
+
+        debug("DB `" + name + " ' loaded successfully")
+
+	return tree
 
     def save(self, name, tree) :
 	assert(name != None)
@@ -139,11 +144,11 @@ class Database :
 
 	try :
 	    xml = ET.Element(tree.text)
-            assert(xml != None)
+	    assert(xml != None)
 	    toxml(tree, xml)
-            assert(tree != None)
+	    assert(tree != None)
 
-            #ET.dump(xml)
+	    ET.dump(xml)
 
 	    root = ET.ElementTree(xml)
 	    root.write(name)
@@ -156,6 +161,8 @@ class Database :
 				      "`" + name + "' (" + str(e) + ")")
 	except :
 	    bug()
+
+        debug("DB `" + name + " ' saved successfully")
 
 # Test
 if (__name__ == '__main__') :
