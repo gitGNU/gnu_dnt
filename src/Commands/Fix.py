aa49273e1975d4994b2e1149d00271fb4909e769
Francesco Salvestrini <salvestrini@gmail.com>
2008-12-20 00:13:30 +0100

    Added fix command

---
+++ src/Commands/Fix.py
@@ -0,0 +1,59 @@
+#
+# Copyright (C) 2008 Francesco Salvestrini
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+import sys
+
+from   Debug      import *
+from   Trace      import *
+from   Command    import *
+import Exceptions
+import DB
+from   ID         import *
+import Entry
+import Tree
+
+def description() :
+    return "fix database"
+
+def do(configuration, arguments) :
+    command = Command("fix")
+
+    (opts, args) = command.parse_args(arguments)
+
+    # Parameters setup
+
+    db_file = configuration.get(PROGRAM_NAME, 'database')
+
+    # Work
+    db = DB.Database()
+
+    # Load database from file
+    tree = db.load(db_file)
+    assert(tree != None)
+
+    #tree.dump("")
+
+    # Save database back to file
+    db.save(db_file, tree)
+
+    debug("Success")
+
+# Test
+if (__name__ == '__main__') :
+    debug("Test completed")
+    sys.exit(0)
