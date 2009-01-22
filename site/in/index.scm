ff02166e971b0e4731078d84ce54fa586f0e3df7
Francesco Salvestrini <salvestrini@gmail.com>
2009-01-22 19:04:10 +0100

    Updated site

--- site/in/index.scm
+++ site/in/index.scm
@@ -1,6 +1,54 @@
 (h2 "DESCRIPTION")
 
-(h3 "Use")
+(p "DNT is a Development (Note) Tracker with a command line user interface. "
+   "It is somewhat similar to "
+   (a (@ (href "http://swapoff.org/DevTodo")) "devtodo")
+   " with a plugin-oriented design.")
+(p "It aims to ease the small/medium projects management tasks (bugs "
+   "tracking, feature requests, notes etc) by keeping all the required "
+   "information together in the same project's source repository. ")
+(p "Being a command line oriented utility it has, as a side-effect, the "
+   "ability to automatize the generation of BUGS, NEWS, TODOs etc. files.")
+
+(h2 "INSTALLATION")
+
+(h3 "Prerequisites")
+(p "In order to install DNT into your system you will need:"
+(ul
+ (li (a (@ (href "http://www.python.org")) "Python > 2.5") )
+ )
+)
+
+(h3 "Installation")
+(p "The following notes do not override your system's package management "
+   "software, please follow your system's guidelines to install the software.")
+
+(p "DNT uses the autotools facilities, the simplest way to install "
+   "the package in your system is:")
+(ul
+ (li (kbd "cd") " to the directory containing the package's source code "
+     "and type " (kbd "./configure") " to configure the package for your "
+     "system."
+     "Running " (kbd "configure") " might take a while. While running, "
+     "it prints some messages telling which features it is checking for")
+ (li "Type " (kbd "make") " to compile the package")
+ (li "Optionally, type " (kbd "make check") " to run all self-tests that "
+     "come with the package")
+ (li "Type " (kbd "make install") " to install the programs and any data "
+     "files and documentation"))
+
+(p "Use " (kbd "./configure --help") " to get a brief help of all "
+   "configuration tweaks available.")
+(p "You can remove the program binaries and object files from the "
+   "source code directory by typing " (kbd "make clean") ". "
+   "To also remove the files that " (samp "configure") " created (so you "
+   "can compile the package for a different architecture), type "
+   (kbd "make distclean") ".")
+(p "You can also type " (kbd "make uninstall") " to remove the installed "
+   "files.")
+(p "Note that the " (samp "--prefix") " option allows installation using "
+   "different paths than the default ones, this option enables sandbox "
+   "installations.")
 
 (h2 "COPYING")
 (p "The project is licensed under the "
