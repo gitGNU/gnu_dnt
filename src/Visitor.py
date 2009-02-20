c4cca6ffacbd4561ae39033a5f49bc8b534add6e
Francesco Salvestrini <salvestrini@gmail.com>
2009-02-20 15:59:08 +0100

    Added level_previous() and level_current() into Visitor class

--- src/Visitor.py
+++ src/Visitor.py
@@ -26,9 +26,10 @@ import Root
 import Tree
 
 class Visitor(object) :
-    def __init__(self, level = 0, index = 0) :
-        self.__level = level
-        self.__index = index
+    def __init__(self, level_current = 0, index = 0, level_previous = 0) :
+        self.__level_current  = level_current
+        self.__level_previous = level_previous
+        self.__index          = index
         debug("Visitor initialized")
 
 # The following method must be provided by the subclass
@@ -39,8 +40,11 @@ class Visitor(object) :
 #    def visitRoot(self, r) :
 #        bug()
 
-    def level(self) :
-        return self.__level
+    def level_previous(self) :
+        return self.__level_previous
+
+    def level_current(self) :
+        return self.__level_current
 
     def index(self) :
         return self.__index
@@ -53,19 +57,29 @@ class Visitor(object) :
         else :
             bug("Unknown type " + str(type(n)) + " for Visitor")
 
-        old_level = self.__level
-        self.__level = self.__level + 1
-        index = 0
+        old_level            = self.__level_current
+        self.__level_current = self.__level_current + 1
+        index                = 0
         for j in n.children() :
-            index = index + 1
+            index        = index + 1
             self.__index = index
             # Please, re-accept myself ;-)
             j.accept(self)
-        self.__level = old_level
+        self.__level_current = old_level
 
 # Test
 if (__name__ == '__main__') :
     v = Visitor()
+
+    class V(Visitor) :
+        def __init__(self) :
+            super(Visitor, self).__init__()
+
+        def visitRoot(self, e) :
+            pass
+
+        def visitEntry(self, e) :
+            pass
 
     debug("Test completed")
     sys.exit(0)
