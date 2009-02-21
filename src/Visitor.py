b409dd832fc7e5adde3c7d895a31c976ba03caf9
Francesco Salvestrini <salvestrini@gmail.com>
2009-02-21 08:52:56 +0100

    Added level_previous() and level_current() methods on Visitor class

--- src/Visitor.py
+++ src/Visitor.py
@@ -50,6 +50,8 @@ class Visitor(object) :
         return self.__index
 
     def visit(self, n) :
+        debug("Visiting " + str(n))
+
         if (type(n) == Root.Root) :
             self.visitRoot(n)
         elif (type(n) == Entry.Entry) :
@@ -57,8 +59,11 @@ class Visitor(object) :
         else :
             bug("Unknown type " + str(type(n)) + " for Visitor")
 
-        old_level            = self.__level_current
-        self.__level_current = self.__level_current + 1
+        assert(hasattr(n, "children"))
+
+        old_level             = self.__level_current
+        self.__level_previous = self.__level_current
+        self.__level_current  = self.__level_current + 1
         index                = 0
         for j in n.children() :
             index        = index + 1
@@ -69,17 +74,56 @@ class Visitor(object) :
 
 # Test
 if (__name__ == '__main__') :
-    v = Visitor()
+    import Entry
 
     class V(Visitor) :
-        def __init__(self) :
-            super(Visitor, self).__init__()
+        def __init__(self, data) :
+            super(V, self).__init__()
+            self.__data = data
 
         def visitRoot(self, e) :
-            pass
+            debug("Visiting Root " + str(e))
+            self.__data.append(self.level_previous())
+            self.__data.append(self.level_current())
 
         def visitEntry(self, e) :
-            pass
+            debug("Visiting Entry " + str(e))
+            self.__data.append(self.level_previous())
+            self.__data.append(self.level_current())
 
-    debug("Test completed")
+    debug("Running")
+    root = Entry.Entry("root")
+    e1   = Entry.Entry("e1")
+    e11  = Entry.Entry("e11")
+    e12  = Entry.Entry("e12")
+    e2   = Entry.Entry("e2")
+
+    debug("Building tree")
+    e1.child(0, e12)
+    e1.child(0, e11)
+    root.child(0, e1)
+    root.child(1, e2)
+
+    debug(str(root))
+
+    debug("Visiting")
+    l = []
+    v = V(l)
+    debug(str(v))
+    debug(type(v))
+
+    debug("Accepting")
+    root.accept(v)
+
+    # [0, 0, 0, 1, 0, 2, 0, 2, 0, 1]
+    t = [0, 0, 0, 1, 1, 2, 2, 2, 2, 1]
+
+    debug(l)
+    debug(t)
+
+    if (t != l) :
+        debug("failed")
+        sys.exit(1)
+
     sys.exit(0)
+    debug("Test completed")
