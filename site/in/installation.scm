faa904790147268479b1b3afb3b2a171d8173675
Francesco Salvestrini <salvestrini@gmail.com>
2009-03-05 22:39:28 +0100

    Fixed site typos

--- site/in/installation.scm
+++ site/in/installation.scm
@@ -1,7 +1,7 @@
 (h2 "INSTALLATION")
 
 (h3 "Prerequisites")
-(p "In order to install " ,as_site_variable_PROGRAMNAME " into your system "
+(p "In order to install the package into your system "
    "you will need:"
    (ul
     (li (a (@ (href "http://www.python.org")) "Python > 2.5") )
@@ -40,4 +40,3 @@
 (p "Note that the " (samp "--prefix") " option allows installation using "
    "different paths than the default ones, this option enables sandbox "
    "installations.")
-
