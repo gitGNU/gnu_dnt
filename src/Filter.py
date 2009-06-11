bdb43ab86ea1b0b81b5d0fb4c9d6cbce42a4a2a5
Francesco Salvestrini <salvestrini@gmail.com>
2009-06-12 00:46:16 +0200

    Removed moved code

--- src/Filter.py
+++ src/Filter.py
@@ -23,6 +23,7 @@ import re
 from   Debug      import *
 from   Trace      import *
 import Entry
+import Expression
 import Exceptions
 
 def help() :
@@ -32,53 +33,8 @@ class Filter(object) :
     def __init__(self, s = None) :
         if (s == None) :
             s = "all"
-        self.__function = self._parse(s)
+        self.__function = Expression.Expression(s).function
         assert(self.__function != None)
-
-    def _parse(self, s) :
-        assert(s != None)
-
-        t = s
-        debug("Filter is `" + str(s) + "'")
-
-        # Replace ',' with 'and'
-        t = t.replace(",", " and ")
-        # Replace '&' with 'and'
-        t = t.replace("&", " and ")
-        # Replace '|' with 'or'
-        t = t.replace("|", " or ")
-        debug("Filter is now `" + str(t) + "'")
-
-        # Remove useless spaces
-        t = re.sub(r'\s+', ' ', t)
-
-        # Then split using ' '
-        t = t.split(" ")
-        debug("Filter is now `" + str(t) + "'")
-
-        # Some consistency checks
-        for i in t :
-            assert(i != None)
-            assert(i != "")
-
-        tmp = None
-        for i in t :
-            debug("Handling filter `" + i + "'")
-            if (i == "all") :
-                tmp = lambda x: True
-            elif (i == "done") :
-                tmp = lambda x: x.done()
-            elif (i == "not-done") :
-                tmp = lambda x: not(x.done())
-            elif (i == "and") :
-                tmp = lambda x, y: x and y
-            elif (i == "or") :
-                tmp = lambda x, y: x or y
-            else :
-                raise Exceptions.UnknownFilter(s)
-        assert(tmp != None)
-
-        return tmp
 
     def function_get(self) :
         assert(self.__function != None)
