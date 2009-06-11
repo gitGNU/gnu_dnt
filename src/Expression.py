41fc3838db39c7a1aaae4d99fcdc7ee5ef227be7
Alessandro Massignan <ff0000.it@gmail.com>
2009-06-12 00:45:32 +0200

    Moved Filter code into Expression

--- src/Expression.py
+++ src/Expression.py
@@ -18,20 +18,90 @@
 
 import sys
 import string
+import re
 
 from   Debug      import *
 from   Trace      import *
 import Exceptions
 
 class Expression(object) :
-    def __init__(self, e = "") :
-        self._expression = self._parse(e)
+    def __init__(self, s = "") :
+        self.__function = self._parse(s)
 
-    def _parse(self, e) :
-        raise Exception.InvalidExpression(e)
+    def _parse(self, s) :
+        assert(s != None)
+
+        t = s
+        debug("Expression is `" + str(s) + "'")
+
+        # Replace ',' with 'and'
+        t = t.replace(",", " and ")
+        # Replace '&' with 'and'
+        t = t.replace("&", " and ")
+        # Replace '|' with 'or'
+        t = t.replace("|", " or ")
+        debug("Expression is now `" + str(t) + "'")
+
+        # Remove useless spaces
+        t = re.sub(r'\s+', ' ', t)
+
+        # Then split using ' '
+        t = t.split(" ")
+        debug("Expression is now `" + str(t) + "'")
+
+        # Some consistency checks
+        for i in t :
+            assert(i != None)
+            assert(i != "")
+
+        tmp = None
+        for i in t :
+            debug("Handling expression `" + i + "'")
+            if (i == "all") :
+                tmp = lambda x: True
+            elif (i == "done") :
+                tmp = lambda x: x.done()
+            elif (i == "not-done") :
+                tmp = lambda x: not(x.done())
+            elif (i == "and") :
+                tmp = lambda x, y: x and y
+            elif (i == "or") :
+                tmp = lambda x, y: x or y
+            else :
+                raise Exceptions.InvalidExpression(s)
+        assert(tmp != None)
+
+        return tmp
+
+    def function_get(self) :
+        assert(self.__function != None)
+        return self.__function
+
+    function = property(function_get, None)
 
 # Test
 if (__name__ == '__main__') :
+
+    v = Expression("all")
+    assert(v != None)
+    v = Expression("done")
+    assert(v != None)
+    v = Expression("not-done")
+    assert(v != None)
+    v = Expression("all,done,not-done")
+    assert(v != None)
+    v = Expression("all, done, not-done")
+    assert(v != None)
+    v = Expression("all ,done ,not-done")
+    assert(v != None)
+    v = Expression("all , done , not-done")
+    assert(v != None)
+    v = Expression("all  ,  done  ,  not-done")
+    assert(v != None)
+    v = Expression("all   ,   done   ,   not-done")
+    assert(v != None)
+    v = Expression("all    ,    done    ,    not-done")
+    assert(v != None)
 
 #    # The following tests should PASS
 #    try :
